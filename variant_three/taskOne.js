44887a55a4ec7fdbc619e94387c13f0b386837d0
Egor Pavlovich <ambidexterdexter@gmail.com>
2022-01-19 10:32:19 +0300

    Added two modules
    I added two files "taskOne.js" and "taskTwo.js" with a description of an individual task

---
+++ variant_three/taskOne.js
@@ -0,0 +1,37 @@
+/*Вариант 3*/
+
+/*Реализовать функцию, которая принимает массив строк и удаляет все последовательные повторяющиеся буквы из каждой строки в массиве. 
+Строки должны быть только строчными, без пробелов.*/
+
+
+/*Пример*/ 
+
+//dup(["abracadabra","allottee","assessee"]) 
+// -> ["abracadabra","alote","asese"] 
+
+//dup(["kelless","keenness"]) 
+// -> ["keles","kenes"]
+
+
+/*Дополнительные тесты*/ 
+
+//dup([ "ccooddddddewwwaaaaarrrrsssss", "piccaninny", "hubbubbubboo" ]) 
+// -> ['codewars','picaniny','hubububo'] 
+
+//dup(["abracadabra","allottee","assessee"]) 
+// -> ['abracadabra','alote','asese'] 
+
+//dup(["kelless","keenness"]) 
+// -> ['keles','kenes'] 
+
+//dup(["adanac","soonness","toolless","ppellee"]) 
+// -> ['adanac','sones','toles','pele'] 
+
+//dup(["callalloo","feelless","heelless"]) 
+// -> ['calalo','feles','heles'] 
+
+//dup(["kelless","voorraaddoosspullen","achcha"]) 
+// -> ['keles','voradospulen','achcha']
+
+
+// Решение:
